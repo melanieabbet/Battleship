e2a9c39d1a64ad533e4e50f931452e4e0928ee99
Mélanie Abbet <melanieabbet97@gmail.com>
2025-03-30 22:43:09 +0200

    multithreading local

--- network.py
+++ network.py
@@ -1,5 +1,5 @@
 import socket
-
+import threading
 
 class Server:
     '''
@@ -12,16 +12,8 @@ class Server:
         #socket.AF_INET is for IPV4
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def run(self):
-        self.server.bind((self.host, self.port))
-        self.server.listen(1)
-        print(f"Listening on {self.host}:{self.port}")
-
-        #Accept connection
-        client_conn, client_addr = self.server.accept()
-        with client_conn:
-            print('Connection', client_addr)
-            print(f"Connection {client_addr[0]}:{client_addr[1]}")
+    def handle_client(self, client_conn, client_addr):
+        try:
             #Communication loop
             while True:
                 request = client_conn.recv(1024)
@@ -31,16 +23,34 @@ class Server:
                 if request.lower() == "close":
                     client_conn.send("closed".encode("utf-8"))
                     break
-
+                #Not closing - get request
                 print(f"Received: {request}")
 
                 #Responding
                 response = "accepted".encode("utf-8") # convert string to bytes
                 client_conn.send(response)
-
+        except Exception as e:
+            print(f"Error when hanlding client: {e}")
+        finally:
             # on break close connection with the client
             client_conn.close()
-            print("Connection to client closed")
+            print(f"Connection to client ({client_addr[0]}:{client_addr[1]}) closed")
+    
+    def run(self):
+        try:
+            self.server.bind((self.host, self.port))
+            self.server.listen(0)
+            print(f"Listening on {self.host}:{self.port}")
+
+            while True:
+                #Accept connection
+                client_conn, client_addr = self.server.accept()
+                print(f"Connection {client_addr[0]}:{client_addr[1]}")
+                thread = threading.Thread(target=self.handle_client, args=(client_conn, client_addr,))
+                thread.start()
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
             self.server.close()
 
 
@@ -55,23 +65,27 @@ class Client:
     def run(self):
         self.client.connect((self.host,self.port))
 
-        while True:
-            # input & send message
-            message = input("Enter message: ")
-            self.client.send(message.encode("utf-8")[:1024])
-        
-            # response
-            response = self.client.recv(1024)
-            response = response.decode("utf-8")
+        try:
+            while True:
+                # input & send message
+                message = input("Enter message: ")
+                self.client.send(message.encode("utf-8")[:1024])
+            
+                # response server
+                response = self.client.recv(1024)
+                response = response.decode("utf-8")
 
-            # if server sent us "closed" in the payload, we break out of the loop and close our socket
-            if response.lower() == "closed":
-                break
+                # if server sent us "closed" in the payload, we break out of the loop and close our socket
+                if response.lower() == "closed":
+                    break
 
-        print(f"Received: {response}")
-        # close client socket (connection to the server)
-        self.client.close()
-        print("Connection to server closed")
+                print(f"Received: {response}")
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:       
+            # close client socket (connection to the server)
+            self.client.close()
+            print("Connection to server closed")
 
 
 # Tests terminal 1:
@@ -79,5 +93,5 @@ class Client:
 # test_server.run()
 
 # Tests terminal 2:
-# test_server = Server()
-# test_server.run()
+# test_client1 = Client()
+# test_client1.run()
