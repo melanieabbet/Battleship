afc89e3b66d385dd4bf8fe26aed9d9c9d72cffc4
Mélanie Abbet <melanieabbet97@gmail.com>
2025-04-08 19:05:11 +0200

    Delete french texts

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
 '''execution module to test as a playground'''
 import time
-from enum import Enum
 from gameplay import Player
-
-    
-
 
 if __name__ =="__main__":
     # 1) start of the game and setup
     name = input("Enter your name:")
     player = Player(name)
-    # enemy = Player("enemy")
-
-    # player.terminal.print_grid(player.grid, enemy.grid)
-    
     player.set_role()
 
     # 2) whait for oponent
     player.lobby()
-
     time.sleep(2)
 
-    # 3) place all the boat
+    # 3) set all the boat
     player.set_boat()
 
     # 4) game start
@@ -31,6 +22,5 @@ if __name__ =="__main__":
         time.sleep(3)
 
         if result in ["win", "lose"]:
-            break  # Fin du jeu
+            break  # End of game
     
-    # 5) TODO end of game // restart?
